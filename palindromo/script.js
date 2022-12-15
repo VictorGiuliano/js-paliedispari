c8959b578343929a55d45f4d59738544f58e29d0
VictorGiuliano <victorgiuliano98@gmail.com>
2022-12-15 17:13:53 +0100

    Raccolta parola dall'utente

--- palindromo/script.js
+++ palindromo/script.js
@@ -1,2 +1,18 @@
 /*Chiedere all’utente di inserire una parola tramiite un form
 Creare una funzione per capire se la parola inserita è palindroma*/
+
+const form = document.getElementById('word');
+const button = document.getElementById('button');
+const result = document.getElementById('respose');
+
+function answer(wordUser){
+
+    } 
+
+button.addEventListener('click', function(){
+    const wordUser =  form.value.trim(); 
+    if(!wordUser){
+        result.innerText = "Non hai inserito la parola";
+    } console.log(wordUser);  
+})
+
