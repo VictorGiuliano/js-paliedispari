b4e20180ce8776362ea7b3adda2e937bc164b456
VictorGiuliano <victorgiuliano98@gmail.com>
2022-12-22 15:44:40 +0100

    Complete pari e dispari

--- ParieDispari/script.js
+++ ParieDispari/script.js
@@ -3,6 +3,15 @@ const numberField = document.getElementById('number');
 const choiceField = document.getElementById('choice');
 const result = document.getElementById('result');
 
+function getRandomNumber(min,max){
+    return Math.floor(Math.random()* (max + 1 - min ))+min;
+}
+
+function isEven(number){
+    return number % 2 === 0;
+}
+
+
 form.addEventListener('submit',function (event){
     event.preventDefault();
 
@@ -12,7 +21,23 @@ form.addEventListener('submit',function (event){
 
 
 
-    if(isNaN(number)||number<1||number>5){
+    if(isNaN(userNumber) || userNumber < 1 ||userNumber>5){
         alert('Il numero inserito non è valido');
     }
+    if(!userChoice || (userChoice !== 'even' && userChoice !== 'odd')){
+        alert('La scelta non è valida');
+        return;
+    }
+
+    const cpuNumber = getRandomNumber(1,5);
+    console.log(cpuNumber);
+
+    const sum = userNumber + cpuNumber;
+    console.log(sum);
+
+    const rightChoice = isEven(sum) ? 'even'  : 'odd';
+
+    const winner = userChoice  === rightChoice ? 'User' : 'Cpu';
+
+    result.innerText = 'The winner is ' + winner;
 })
